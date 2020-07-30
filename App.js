35f641b6cd15f8b89a22dd161527d5fc66293022
Thales Macêdo <thalesmacedo1@live.com>
2020-07-30 00:20:34 -0300

    Display + Button implementation

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, Dimensions } from 'react-native'
+
+import Button from './src/components/Button'
+import Display from './src/components/Display'
 
 export default function App() {
+  const [displayValue, setDisplayValue] = useState(0)
+
+  const addDigit = n => {
+    setDisplayValue(n)
+  }
+
+  const clearMemory = () => {
+    setDisplayValue(0)
+  }
+
+  const setOperation = operation => {
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Display value={displayValue} />
+      <View style={styles.buttons}>
+        <Button label='AC' triple onClick={clearMemory} />
+        <Button label='/' operator onClick={setOperation} />
+        <Button label='7' onClick={addDigit} />
+        <Button label='8' onClick={addDigit} />
+        <Button label='9' onClick={addDigit} />
+        <Button label='*' operator onClick={setOperation} />
+        <Button label='4' onClick={addDigit} />
+        <Button label='5' onClick={addDigit} />
+        <Button label='6' onClick={addDigit} />
+        <Button label='-' operator onClick={setOperation} />
+        <Button label='1' onClick={addDigit} />
+        <Button label='2' onClick={addDigit} />
+        <Button label='3' onClick={addDigit} />
+        <Button label='+' operator onClick={setOperation} />
+        <Button label='0' double onClick={addDigit} />
+        <Button label='.' onClick={addDigit} />
+        <Button label='=' operator onClick={setOperation} />
+      </View>
     </View>
   );
 }
@@ -14,8 +48,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  }
 });
